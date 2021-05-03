d41a76ab11cd9a6c6836a932619aea9bbf588250
fra9106 <contact@monpersoweb.fr>
2021-05-03 19:10:48 +0200

    config heroku5

--- swagger/swagger.php
+++ swagger/swagger.php
@@ -5,7 +5,7 @@ use OpenApi\Annotations as OA;
 /**
  * @OA\Info(title="api-bilmo", version="0.1")
  * @OA\Server(
- *     url="http://localhost:8000/api", 
+ *     url="http://api-from-scratch-symfony.herokuapp.com/api", 
  *     description="Api BileMo"
  * )
  * 
